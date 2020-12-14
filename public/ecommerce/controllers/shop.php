3e4ebb1d9634595155f33b4caee8cf9929ae7f87
gabivlj <gabitriqui@gmail.com>
2020-12-14 02:15:50 +0100

    super dirty way of managing product quantities...

--- public/ecommerce/controllers/shop.php
+++ public/ecommerce/controllers/shop.php
@@ -17,10 +17,50 @@ class Shop extends Controller
 
   function remove_item()
   {
+    $itemID = App::$uri_params['item_id'];
+    $cart = new Model('cart_item');
+    $cartItem = $cart
+      ->Select('quantity')
+      ->Where(['item_id=' => $itemID])
+      ->And(['user_id=' => Items::$user['id']])
+      ->Do();
+    if (!$cartItem) {
+      Items::render("./public/ecommerce/html/not_found.html");
+      return;
+    }
+    $cartItem = $cartItem[0];
+    // Select the item (we want the type and the quantity)
+    $item = (new Model('items'))->Select('type, quantity')->Where(['id_ext=' => $itemID])->Do();
+    if (!$item) {
+      Items::render("./public/ecommerce/html/not_found.html");
+      return;
+    }
+    $item = $item[0];
+    // Update quantities to the stock
+    if (!(new Model('items'))
+      ->Update()
+      ->Where(['id_ext=' => $itemID])
+      ->Set(['quantity' => $item['quantity'] + $cartItem['quantity']])
+      ->Do()) {
+      Items::render("./public/ecommerce/html/not_found.html");
+    }
+    // Delete from cart
+    $ok = $cart
+      ->Delete()
+      ->Where(['item_id=' => $itemID])
+      ->And(['user_id=' => Items::$user['id']])
+      ->Do();
+    if (!$ok) {
+      App::set_response_header('location', "/shop/{$item['type']}/{$itemID}?error=3");
+    } else {
+      App::set_response_header('location', "/shop/{$item['type']}/{$itemID}");
+    }
   }
 
   function get_item()
   {
+    // Clean items
+    $this->cleanItems();
     QueryOptions::$DEBUG_QUERIES = false;
     $type = App::$uri_params['type'];
     if (!isset(Items::$available_types[$type])) {
@@ -67,8 +107,7 @@ class Shop extends Controller
     $itemID = App::$uri_params['item_id'];
     $item = (new Model('items'))->Select('*')->Where(['id_ext=' => $itemID])->Limit(1)->Do();
     if (!$item) {
-      App::status_code(404);
-      App::json(['message' => 'Item does not exist']);
+      Items::render("./public/ecommerce/html/not_found.html");
       return;
     }
     $item = $item[0];
@@ -84,7 +123,11 @@ class Shop extends Controller
     // New quantity
     $newQuantity = $item['quantity'] - $body['quantity'];
     // Update quantity in stock
-    if (!(new Model('items'))->Update()->Where(['id_ext=' => $itemID])->Set(['quantity' => $newQuantity])->Do()) {
+    if (!(new Model('items'))
+      ->Update()
+      ->Where(['id_ext=' => $itemID])
+      ->Set(['quantity' => $newQuantity])
+      ->Do()) {
       App::set_response_header('location', "/shop/{$item['type']}/{$itemID}?error=2");
       return;
     }
@@ -128,5 +171,28 @@ class Shop extends Controller
     }
     $id = $_SESSION['id'];
     Items::$user = User::getById($id);
+  }
+
+  function cleanItems()
+  {
+    // 30 MINS
+    $SECONDS_EXPIRES = 60 * 30;
+    $item = new Model('cart_item');
+    $rows = $item
+      ->Select('added, quantity, item_id')
+      ->Where(['added<' => time() - $SECONDS_EXPIRES])
+      ->Do();
+    if (!$rows) return;
+    foreach ($rows as $row) {
+      (new Model('items'))
+        ->Update()
+        ->Set(['quantity' => new Name("quantity + {$row['quantity']}")])
+        ->Where(['id_ext=' => $row['item_id']])
+        ->Do();
+    }
+    $item
+      ->Delete('added, quantity, item_id')
+      ->Where(['added<' => time() - $SECONDS_EXPIRES])
+      ->Do();
   }
 }
